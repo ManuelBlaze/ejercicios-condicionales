1d4a17de57e96bdd8d9587dd82c83b3933b70852
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-06 16:07:09 -0500

    Cambios finales

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/5bd7cc0966.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="body.css">
     <title>Ejercicios Condicionales</title>
@@ -13,31 +14,35 @@
 <body>
     <h1>Manuel Alejandro Escobar Mira - Grupo 2</h1>
     <div class="enlace">
-        <a href="Ej-1.html" target="_blank">Ejercicio 1</a>
+        <a href="Ej-1.html">Ejercicio 1</a>
     </div>
     <div class="enlace">
-        <a href="Ej-2.html" target="_blank">Ejercicio 2</a>
+        <a href="Ej-2.html">Ejercicio 2</a>
     </div>
     <div class="enlace">
-        <a href="Ej-3.html" target="_blank">Ejercicio 3</a>
+        <a href="Ej-3.html">Ejercicio 3</a>
     </div>
     <div class="enlace">
-        <a href="Ej-4.html" target="_blank">Ejercicio 4</a>
+        <a href="Ej-4.html">Ejercicio 4</a>
     </div>
     <div class="enlace">
-        <a href="Ej-5.html" target="_blank">Ejercicio 5</a>
+        <a href="Ej-5.html">Ejercicio 5</a>
     </div>
     <div class="enlace">
-        <a href="Ej-6.html" target="_blank">Ejercicio 6</a>
+        <a href="Ej-6.html">Ejercicio 6</a>
     </div>
     <div class="enlace">
-        <a href="Ej-7.html" target="_blank">Ejercicio 7</a>
+        <a href="Ej-7.html">Ejercicio 7</a>
     </div>
     <div class="enlace">
-        <a href="Ej-8.html" target="_blank">Ejercicio 8</a>
+        <a href="Ej-8.html">Ejercicio 8</a>
     </div>
     <div class="enlace">
-        <a href="Ej-9.html" target="_blank">Ejercicio 9</a>
+        <a href="Ej-9.html">Ejercicio 9</a>
+    </div>
+    <div class="enlace">
+        <a href="https://github.com/ManuelBlaze/ejercicios-condicionales" target="_blank"><i class="fab fa-github"></i>
+            Repositorio</a>
     </div>
 </body>
 
